1fc6ee8179a10905074b049f79a47d63e9f00552
Tommaso <tommaso.lodesani@gmail.com>
2022-05-04 15:08:12 +0200

    setting project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// chiedere il numero di km
+
+const numeroKm = parseInt(prompt("Quanti km devi percorrere?"));
+
+console.log(numeroKm);
+// chiedere l'eta
+// calcolare prezzo tot biglietto
+// SE minorenne applicare sconto del 20%
+// SE over 65 applicare sconto del 40%
